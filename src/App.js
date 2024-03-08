29bae5c82a76f92988ec85fe7f448ace4f3f5577
Кахарман Кашаев <kakharmankashayev@MacBook-Air-Kaharman.local>
2024-03-09 00:58:12 +0500

    project innit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+import logo from '../src/Images/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="App-logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
